34211c2e588782226f8ce7fd8ba3604ddb9b7a2d
clubzspadez <clubzspadez@gmail.com>
2019-01-09 06:41:03 -0500

    working on api, and csv import

---
+++ app/controllers/api/v1/post_controller.rb
@@ -0,0 +1,21 @@
+module Api
+    module V1
+        class PostController < ApplicationController
+            def index
+
+            end
+        
+            def show
+        
+            end
+        
+            def destroy
+        
+            end
+        
+            def update
+        
+            end
+        end
+    end
+end
