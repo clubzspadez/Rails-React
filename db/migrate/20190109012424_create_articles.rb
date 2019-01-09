284f6d9ba9d2c26fffc513aaeaf3578c1d737141
clubzspadez <clubzspadez@gmail.com>
2019-01-09 05:32:46 -0500

    added postgres

--- db/migrate/20190109012424_create_articles.rb
+++ db/migrate/20190109012424_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
-    create_table :articles do |t|
+    create_table :articles, :id => false do |t|
       t.integer :id
       t.string :description
       t.string :author
-      t.array :tags
+      t.string :tags, array: true, default: []
       t.string :created_at
       t.string :updated_at
 
